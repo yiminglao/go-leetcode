0fec4363d0d080098ddab7cb63336e23ba1b7894
Ming <yiming.lao@bcfranchise.com>
2019-12-10 23:48:26 -0700

    length of last word 58

---
+++ length-of-last-word-58/length-of-last-word-58.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	str := strings.Trim("Today is a nice day", " ")
+
+	fmt.Println(lengthOfLastWord(str))
+}
+
+func lengthOfLastWord(s string) int {
+	str := strings.TrimSpace(s)
+	str2 := strings.Split(str, " ")
+	for i := len(str2) - 1; i >= 0; i-- {
+		for _, value := range str2[i] {
+			if !unicode.IsLetter(value) {
+				return 0
+			}
+		}
+		return len(str2[i])
+	}
+	return 0
+}
